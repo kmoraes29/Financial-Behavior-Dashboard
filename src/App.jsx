f3ba29ef132e248703ab972a633b98510f568f6a
Alessandra Nébias <anebias@hotmail.com>
2026-05-06 19:00:38 -0300

    feat: restore project

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import { BsBarChartFill } from "react-icons/bs";
 import './styles/global_styles.css'
+import { Dashboard } from "./components/Dashboard/Dashboard";
+import { AccountsProvider } from "./context/AccountsContext";
+import { TransactionsProvider } from "./context/TransactionsContext";
 
 function App() {
 
   return (
-    <>
-    <BsBarChartFill/>
-    <h1>Sistema Inteligente de Análise de Perfil Financeiro, Consumo e Geração de 
-Insights</h1>
-    </>
+    <AccountsProvider>
+      <TransactionsProvider>
+      <BsBarChartFill/>
+      <Dashboard />
+      </TransactionsProvider>
+    </AccountsProvider>
   )
 }
 
