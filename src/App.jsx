f7ed7b284714af305a9e5e2a44c4b45ec82752e5
Alessandra Nébias <anebias@hotmail.com>
2026-05-08 10:43:05 -0300

    correção

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import Navbar from "./components/navbar";
 import Overview from "./components/overview";
 import DashboardGraphs from "./components/DashboardGraphs/dashboardGraphs";
-import { Dashboard } from "./components/Dashboard/Dashboard";
 import { AccountsProvider } from "./context/AccountsContext";
 import { TransactionsProvider } from "./context/TransactionsContext";
 import { BalancesProvider } from "./context/BalancesContext";
 import { CompaniesProvider } from "./context/EmpresaContext";
 import { PeriodsProvider } from "./context/PeriodsContext";
-import { MetricCards } from "./components/MetricCards";
 
 function App() {
   return (
